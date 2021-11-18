5e7300aa4b8b1c4532822f7c3472fd9046a35c74
Petro Lehtonen <petro.lehtonen@gmail.com>
2021-11-18 16:36:07 +0200

    viilattu kirjautumistoiminnallisuutta

--- routes.py
+++ routes.py
@@ -1,7 +1,17 @@
 from app import app
-from flask import render_template
-from db import db
+from flask import render_template, redirect, request
+import users
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    if users.login(username, password):
+        return redirect("/")
+    else:
+        return "pöö"
--- users.py
+++ users.py
@@ -1,8 +1,29 @@
-from db import db
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask import session
+from db import db
 
-def login(username, password) -> boolean:
-    # testikäyttäjä
-    if username == "testi" and password == "salasana":
+def login(username, password) -> bool:
+    sql = "select id, password, is_admin FROM users WHERE username = :username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if not user:
+        return False
+    if check_password_hash(user.password, password):
+        session["user_id"] = user.id
+        session["is_admin"] = user.is_admin
         return True
     return False
+
+def create(username, password) -> bool:
+    hash_value = generate_password_hash(password)
+    try:        
+        sql = "INSERT INTO users (username, password, is_admin) VALUES (:username, :password, :admin)"
+        db.session.execute(sql, {"username":username, "password":hash_value, "admin":(username=="admin")})
+        db.session.commit()
+    except:
+        return False
+    return login(username, password)
+
+def logout():
+    del session["is_admin"]
+    del session["user_id"]
