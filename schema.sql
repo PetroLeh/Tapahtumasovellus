38e795fd2b9e0e1644968f1ba408e35c159caa21
Petro Lehtonen <petro.lehtonen@gmail.com>
2021-11-18 22:14:07 +0200

    päivitetty sql-skeema

--- schema.sql
+++ schema.sql
@@ -4,3 +4,11 @@ CREATE TABLE users (
     password TEXT,
     is_admin BOOLEAN
 );
+
+CREATE TABLE events (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    start_time TIMESTAMP,
+    end_time TIMESTAMP,
+    description TEXT
+)
